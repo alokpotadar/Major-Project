2b4dc26020bd6d36888839dd2454a55b8506289a
Alok Potadar <44139326+alokpotadar@users.noreply.github.com>
2022-03-11 12:18:28 +0530

    Create ru_script.py

---
+++ Simulations_Sine/ru_script.py
@@ -0,0 +1,21 @@
+import subprocess
+
+# subprocess.run((['python','Sine_save_csv.py','08','3']))
+# subprocess.run((['python','Sine_save_csv.py','16','3']))
+# subprocess.run((['python','Sine_save_csv.py','32','3']))
+# subprocess.run((['python','Sine_save_csv.py','64','3']))
+
+subprocess.run((['python','Sine_save_csv.py','08','5']))
+subprocess.run((['python','Sine_save_csv.py','16','5']))
+subprocess.run((['python','Sine_save_csv.py','32','5']))
+subprocess.run((['python','Sine_save_csv.py','64','5']))
+
+subprocess.run((['python','Sine_save_csv.py','08','7']))
+subprocess.run((['python','Sine_save_csv.py','16','7']))
+subprocess.run((['python','Sine_save_csv.py','32','7']))
+subprocess.run((['python','Sine_save_csv.py','64','7']))
+
+subprocess.run((['python','Sine_save_csv.py','08','9']))
+subprocess.run((['python','Sine_save_csv.py','16','9']))
+subprocess.run((['python','Sine_save_csv.py','32','9']))
+subprocess.run((['python','Sine_save_csv.py','64','9']))
