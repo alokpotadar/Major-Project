f383f009b8a37bc8f54feddfbf5f1b20eb7291db
Alok Potadar <44139326+alokpotadar@users.noreply.github.com>
2022-03-11 12:30:10 +0530

    Create ru_script.py

---
+++ Simulations_Triangle/ru_script.py
@@ -0,0 +1,15 @@
+import subprocess
+
+subprocess.run((['python','Triangle_csv.py','08','7']))
+subprocess.run((['python','Triangle_csv.py','16','7']))
+subprocess.run((['python','Triangle_csv.py','32','7']))
+subprocess.run((['python','Triangle_csv.py','64','7']))
+
+# subprocess.run((['python','square_csv.py','08','5']))
+# subprocess.run((['python','square_csv.py','16','5']))
+# subprocess.run((['python','square_csv.py','32','5']))
+# subprocess.run((['python','square_csv.py','64','5']))
+subprocess.run((['python','Triangle_csv.py','08','9']))
+subprocess.run((['python','Triangle_csv.py','16','9']))
+subprocess.run((['python','Triangle_csv.py','32','9']))
+subprocess.run((['python','Triangle_csv.py','64','9']))
